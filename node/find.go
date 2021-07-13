58c74b49f0953f6713fbc9523a29068eaab8d797
gammazero <gammazero@users.noreply.github.com>
2021-07-13 15:47:18 -0700

    handler checks if storage enabled

--- node/find.go
+++ node/find.go
@@ -19,19 +19,46 @@ func (n *Node) GetSingleCidHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	log.Infow("Find cid", "cid", mhCid)
-	// Get Cid from primary storage
-	i, _, _ := n.primary.Get(c)
-	out := map[cid.Cid][]store.IndexEntry{c: i}
 
+	log.Infow("Find cid", "cid", mhCid)
+	var (
+		entries []store.IndexEntry
+		found   bool
+	)
+
+	// Lookup CID in primary storage if primary enabled
+	if n.primary != nil {
+		entries, found, err = n.primary.Get(c)
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// Lookup CID in persistent storage if not found yet and persistent enabled
+	if !found && n.persistent != nil {
+		entries, found, err = n.primary.Get(c)
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+
+	if !found {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	out := map[cid.Cid][]store.IndexEntry{c: entries}
 	err = writeResponse(w, out)
 	if err != nil {
 		fmt.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	// w.WriteHeader(http.StatusOK)
-
+	//w.WriteHeader(http.StatusOK)
 }
 
 func writeResponse(w http.ResponseWriter, r interface{}) error {
