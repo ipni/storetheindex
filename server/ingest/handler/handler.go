8407c7643f9566b508b2958e35de8829fe3d4397
gammazero <gammazero@users.noreply.github.com>
2021-08-18 21:34:52 -0700

    Put useful error message is error response

--- server/ingest/handler/handler.go
+++ server/ingest/handler/handler.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 
 	indexer "github.com/filecoin-project/go-indexer-core"
+	"github.com/filecoin-project/storetheindex/api/v0"
 	"github.com/filecoin-project/storetheindex/internal/providers"
 	logging "github.com/ipfs/go-log/v2"
 )
@@ -30,4 +32,25 @@ func writeResponse(w http.ResponseWriter, body []byte) error {
 	}
 
 	return nil
+}
+
+func writeError(w http.ResponseWriter, statusCode int, err error) {
+	w.WriteHeader(http.StatusBadRequest)
+	if err == nil {
+		return
+	}
+
+	e := v0.Error{
+		Message: err.Error(),
+	}
+	rb, err := json.Marshal(&e)
+	if err != nil {
+		log.Errorw("failed to marshal error response", "err", err)
+		return
+	}
+
+	err = writeResponse(w, rb)
+	if err != nil {
+		log.Errorw("failed writing error response", "err", err)
+	}
 }
