457d05c36cdbb0f4398fe40b2ba909d324b55396
gammazero <gammazero@users.noreply.github.com>
2021-08-27 08:41:34 -0700

    Remove unused interface functions

--- api/v0/ingest/client/interface.go
+++ api/v0/ingest/client/interface.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/filecoin-project/storetheindex/api/v0/ingest/models"
 	"github.com/filecoin-project/storetheindex/config"
-	"github.com/ipfs/go-cid"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 )
 
@@ -14,9 +13,4 @@ type Ingest interface {
 	GetProvider(ctx context.Context, providerID peer.ID) (*models.ProviderInfo, error)
 	ListProviders(ctx context.Context) ([]*models.ProviderInfo, error)
 	Register(ctx context.Context, providerIdent config.Identity, addrs []string) error
-
-	// Sync with a data provider up to latest ID
-	Sync(ctx context.Context, p peer.ID, cid cid.Cid) error
-	// Subscribe to advertisements of a specific provider in the pubsub channel
-	Subscribe(ctx context.Context, p peer.ID) error
 }
