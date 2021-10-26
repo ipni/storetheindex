26edb768899572a6de68274726eaaed7c8d00c6f
Daniel Martí <mvdan@mvdan.cc>
2021-10-26 11:33:35 +0100

    api/v0/ingest: use ipld.LoadSchema API
    Replace the manual schema type spawning and accumulating with LoadSchema.
    Note that we've been careful to keep the schema identical,
    so there are zero generated code changes.
    
    As such, this refactor seems perfectly safe.

--- api/v0/ingest/schema/gen.go
+++ api/v0/ingest/schema/gen.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"os"
 
@@ -12,94 +13,24 @@ import (
 	gengo "github.com/ipld/go-ipld-prime/schema/gen/go"
 )
 
+//go:embed schema.ipldsch
+var schemaSrc []byte
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Printf("Must specify destination directory")
 		os.Exit(1)
 	}
 
-	ts := schema.TypeSystem{}
-	ts.Init()
+	ts, err := ipld.LoadSchemaBytes(schemaSrc)
+	if err != nil {
+		panic(err)
+	}
+
 	adjCfg := &gengo.AdjunctCfg{
 		CfgUnionMemlayout: map[schema.TypeName]string{
 			"Any": "interface",
 		},
 	}
-
-	// Prelude.  (This is boilerplate; it will be injected automatically by the schema libraries in the future, but isn't yet.)
-	ts.Accumulate(schema.SpawnBool("Bool"))
-	ts.Accumulate(schema.SpawnInt("Int"))
-	ts.Accumulate(schema.SpawnFloat("Float"))
-	ts.Accumulate(schema.SpawnString("String"))
-	ts.Accumulate(schema.SpawnBytes("Bytes"))
-	ts.Accumulate(schema.SpawnMap("Map",
-		"String", "Any", true,
-	))
-	ts.Accumulate(schema.SpawnList("List",
-		"Any", true,
-	))
-	ts.Accumulate(schema.SpawnLink("Link"))
-	ts.Accumulate(schema.SpawnUnion("Any",
-		[]schema.TypeName{
-			"Bool",
-			"Int",
-			"Float",
-			"String",
-			"Bytes",
-			"Map",
-			"List",
-			"Link",
-		},
-
-		schema.SpawnUnionRepresentationKinded(map[ipld.Kind]schema.TypeName{
-			ipld.Kind_Bool:   "Bool",
-			ipld.Kind_Int:    "Int",
-			ipld.Kind_Float:  "Float",
-			ipld.Kind_String: "String",
-			ipld.Kind_Bytes:  "Bytes",
-			ipld.Kind_Map:    "Map",
-			ipld.Kind_List:   "List",
-			ipld.Kind_Link:   "Link",
-		}),
-	))
-
-	// List multihash
-	ts.Accumulate(schema.SpawnStruct("EntryChunk", []schema.StructField{
-		schema.SpawnStructField("Entries", "List_Bytes", false, false),
-		schema.SpawnStructField("Next", "Link_EntryChunk", true, false),
-	}, schema.SpawnStructRepresentationMap(map[string]string{})))
-	ts.Accumulate(schema.SpawnLinkReference("Link_EntryChunk", "EntryChunk"))
-	ts.Accumulate(schema.SpawnList("List_String", "String", false))
-	ts.Accumulate(schema.SpawnList("List_Bytes", "Bytes", false))
-
-	// Advertisement
-	ts.Accumulate(schema.SpawnStruct("Advertisement", []schema.StructField{
-		// Previous advertisement.
-		schema.SpawnStructField("PreviousID", "Link_Advertisement", true, false),
-		// Provider ID of the advertisement.
-		schema.SpawnStructField("Provider", "String", false, false),
-		// Addresses, as list of multiaddr strings, to use for content retrieval.
-		schema.SpawnStructField("Addresses", "List_String", false, false),
-		// Advertisement signature.
-		schema.SpawnStructField("Signature", "Bytes", false, false),
-		// Entries with a link to the list of CIDs
-		schema.SpawnStructField("Entries", "Link", false, false),
-		// Context ID for entries.
-		schema.SpawnStructField("ContextID", "Bytes", false, false),
-		// Serialized v0.Metadata for all entries in advertisement.
-		schema.SpawnStructField("Metadata", "Bytes", false, false),
-		// IsRm or Put?
-		schema.SpawnStructField("IsRm", "Bool", false, false),
-	}, schema.SpawnStructRepresentationMap(map[string]string{})))
-	ts.Accumulate(schema.SpawnLinkReference("Link_Advertisement", "Advertisement"))
-
-	// Our types.
-	if errs := ts.ValidateGraph(); errs != nil {
-		for _, err := range errs {
-			fmt.Printf("- %s\n", err)
-		}
-		panic("not happening")
-	}
-
-	gengo.Generate(os.Args[1], "schema", ts, adjCfg)
+	gengo.Generate(os.Args[1], "schema", *ts, adjCfg)
 }
