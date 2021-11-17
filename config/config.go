436984a124cbc6ac7c5bcad3dfe4d0ff249773a6
gammazero <gammazero@users.noreply.github.com>
2021-11-17 00:55:00 -0800

    Recognize absolute paths in config

--- config/config.go
+++ config/config.go
@@ -9,7 +9,7 @@ import (
 	"github.com/mitchellh/go-homedir"
 )
 
-// Config is used to load storetheindex config files
+// Config is used to load config files.
 type Config struct {
 	Identity  Identity  // peer identity
 	Addresses Addresses // addresses to listen on
@@ -20,11 +20,11 @@ type Config struct {
 }
 
 const (
-	// DefaultPathName is the default config dir name
+	// DefaultPathName is the default config dir name.
 	DefaultPathName = ".storetheindex"
 	// DefaultPathRoot is the path to the default config dir location.
 	DefaultPathRoot = "~/" + DefaultPathName
-	// DefaultConfigFile is the filename of the configuration file
+	// DefaultConfigFile is the filename of the configuration file.
 	DefaultConfigFile = "config"
 	// EnvDir is the environment variable used to change the path root.
 	EnvDir = "STORETHEINDEX_PATH"
@@ -36,20 +36,24 @@ var (
 )
 
 // Filename returns the configuration file path given a configuration root
-// directory. If the configuration root directory is empty, use the default one
+// directory. If the configuration root directory is empty, use the default.
 func Filename(configRoot string) (string, error) {
 	return Path(configRoot, DefaultConfigFile)
 }
 
-// Marshal configuration with JSON
+// Marshal configuration with JSON.
 func Marshal(value interface{}) ([]byte, error) {
-	// need to prettyprint, hence MarshalIndent, instead of Encoder
+	// need to prettyprint, hence MarshalIndent, instead of Encoder.
 	return json.MarshalIndent(value, "", "  ")
 }
 
 // Path returns the config file path relative to the configuration root. If an
-// empty string is provided for `configRoot`, the default root is used.
+// empty string is provided for `configRoot`, the default root is used. If
+// configFile is an absolute path, then configRoot is ignored.
 func Path(configRoot, configFile string) (string, error) {
+	if filepath.IsAbs(configFile) {
+		return filepath.Clean(configFile), nil
+	}
 	if configRoot == "" {
 		var err error
 		configRoot, err = PathRoot()
@@ -60,7 +64,7 @@ func Path(configRoot, configFile string) (string, error) {
 	return filepath.Join(configRoot, configFile), nil
 }
 
-// PathRoot returns the default configuration root directory
+// PathRoot returns the default configuration root directory.
 func PathRoot() (string, error) {
 	dir := os.Getenv(EnvDir)
 	if dir != "" {
@@ -69,7 +73,7 @@ func PathRoot() (string, error) {
 	return homedir.Expand(DefaultPathRoot)
 }
 
-// Load reads the json-serialized config at the specified path
+// Load reads the json-serialized config at the specified path.
 func Load(filePath string) (*Config, error) {
 	var err error
 	if filePath == "" {
@@ -89,13 +93,14 @@ func Load(filePath string) (*Config, error) {
 	defer f.Close()
 
 	var cfg Config
-	if err := json.NewDecoder(f).Decode(&cfg); err != nil {
+	if err = json.NewDecoder(f).Decode(&cfg); err != nil {
 		return nil, err
 	}
-	return &cfg, err
+
+	return &cfg, nil
 }
 
-// Save writes the json-serialized config to the specified path
+// Save writes the json-serialized config to the specified path.
 func (c *Config) Save(filePath string) error {
 	var err error
 	if filePath == "" {
@@ -124,7 +129,7 @@ func (c *Config) Save(filePath string) error {
 	return err
 }
 
-// String returns a pretty-printed json config
+// String returns a pretty-printed json config.
 func (c *Config) String() string {
 	b, err := json.MarshalIndent(c, "", "  ")
 	if err != nil {
--- config/indexer.go
+++ config/indexer.go
@@ -6,12 +6,13 @@ const (
 	defaultValueStoreDir  = "valuestore"
 )
 
-// Indexer holds configuration for the indexer core
+// Indexer holds configuration for the indexer core.
 type Indexer struct {
-	// Maximum number of CIDs that cache can hold,0 to disable cache
+	// Maximum number of CIDs that cache can hold. Setting to 0 disables cache.
 	CacheSize int
-	// Directory withing config root where value store is kept
+	// Directory where value store is kept. If this is not an absolute path
+	// then the location is relative to the indexer repo directory.
 	ValueStoreDir string
-	// Type of value store to use
+	// Type of valuestore to use, such as "sti" or "pogreb".
 	ValueStoreType string
 }
