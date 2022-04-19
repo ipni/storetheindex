e1aacfe28cd826b23807175745c615f1b1a2d5cd
Masih H. Derkani <m@derkani.org>
2022-04-19 16:22:06 +0100

    Write `prod` metrics to a dedicated AWS managed prometheus
    Connect the separated prod monitoring stack to write to a dedicated AWS
    managed prometheus instance.

--- deploy/infrastructure/prod/us-east-2/outputs.tf
+++ deploy/infrastructure/prod/us-east-2/outputs.tf
@@ -22,6 +22,10 @@ output "ebs_csi_controller_role_arn" {
   value = module.ebs_csi_controller_role.iam_role_arn
 }
 
+output "monitoring_role_arn" {
+  value = module.monitoring_role.iam_role_arn
+}
+
 output "prod_cid_contact_nameservers" {
   value = aws_route53_zone.prod_external.name_servers
 }
