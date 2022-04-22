5cc85480dcc117059cbfaa3a09f8f38c293e6f75
gammazero <gammazero@users.noreply.github.com>
2022-04-22 11:09:33 -0700

    Unlimited advertisement depth limit by default

--- config/ingest.go
+++ config/ingest.go
@@ -7,12 +7,14 @@ import (
 // Ingest tracks the configuration related to the ingestion protocol.
 type Ingest struct {
 	// The recursion depth limit when syncing advertisements. The value -1
-	// means no limit and zero means use the default value. This prupose is to
-	// prevent overload from endless advertisement chains.
+	// means no limit and zero means use the default value. Limiting the depth
+	// of advertisements can be done if there is a need to prevent an indexer
+	// from ingesting long chains of advertisements.
 	AdvertisementDepthLimit int
 	// The recursion depth limit when syncing advertisement entries. The value
-	// -1 means no limit and zero means use the default value. This prupose is
-	// to prevent overload from endless entries chains.
+	// -1 means no limit and zero means use the default value. The purpose is
+	// to prevent overload from extremely long entry chains resulting from
+	// publisher misconfiguration.
 	EntriesDepthLimit int
 	// How many ingest worker goroutines to spawn. This controls how many
 	// concurrent ingest from different providers we can handle.
@@ -20,8 +22,8 @@ type Ingest struct {
 	// PubSubTopic used to advertise ingestion announcements.
 	PubSubTopic string
 	// StoreBatchSize is the number of entries in each write to the value
-	// store.  Specifying a value less than 2 disables batching. This should
-	// be smaller than the maximum number of multihashes in an entry block to
+	// store. Specifying a value less than 2 disables batching. This should be
+	// smaller than the maximum number of multihashes in an entry block to
 	// write concurrently to the value store.
 	StoreBatchSize int
 	// SyncTimeout is the maximum amount of time allowed for a sync to complete
@@ -34,7 +36,7 @@ type Ingest struct {
 // NewIngest returns Ingest with values set to their defaults.
 func NewIngest() Ingest {
 	return Ingest{
-		AdvertisementDepthLimit: 65536,
+		AdvertisementDepthLimit: -1,
 		EntriesDepthLimit:       65536,
 		IngestWorkerCount:       10,
 		PubSubTopic:             "/indexer/ingest/mainnet",
