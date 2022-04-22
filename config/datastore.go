4acf797fa57c148f833faedcc268a8d2a7567a21
gammazero <gammazero@users.noreply.github.com>
2022-04-21 22:53:53 -0700

    Alphabetic order makes config items easier to find

--- config/datastore.go
+++ config/datastore.go
@@ -2,19 +2,19 @@ package config
 
 // Datastore tracks the configuration of the datastore.
 type Datastore struct {
-	// Type is the type of datastore.
-	Type string
 	// Dir is the directory where the datastore is kept. If this is not an
 	// absolute path then the location is relative to the indexer repo
 	// directory.
 	Dir string
+	// Type is the type of datastore.
+	Type string
 }
 
 // NewDatastore returns Datastore with values set to their defaults.
 func NewDatastore() Datastore {
 	return Datastore{
-		Type: "levelds",
 		Dir:  "datastore",
+		Type: "levelds",
 	}
 }
 
@@ -22,10 +22,10 @@ func NewDatastore() Datastore {
 func (c *Datastore) populateUnset() {
 	def := NewDatastore()
 
-	if c.Type == "" {
-		c.Type = def.Type
-	}
 	if c.Dir == "" {
 		c.Dir = def.Dir
+	}
+	if c.Type == "" {
+		c.Type = def.Type
 	}
 }
--- config/indexer.go
+++ config/indexer.go
@@ -8,23 +8,23 @@ import (
 type Indexer struct {
 	// Maximum number of CIDs that cache can hold. Setting to 0 disables cache.
 	CacheSize int
+	// GCInterval configures the garbage collection interval for valuestores
+	// that support it.
+	GCInterval Duration
 	// Directory where value store is kept. If this is not an absolute path
 	// then the location is relative to the indexer repo directory.
 	ValueStoreDir string
 	// Type of valuestore to use, such as "sth" or "pogreb".
 	ValueStoreType string
-	// GCInterval configures the garbage collection interval for valuestores
-	// that support it.
-	GCInterval Duration
 }
 
 // NewIndexer returns Indexer with values set to their defaults.
 func NewIndexer() Indexer {
 	return Indexer{
 		CacheSize:      300000,
+		GCInterval:     Duration(30 * time.Minute),
 		ValueStoreDir:  "valuestore",
 		ValueStoreType: "sth",
-		GCInterval:     Duration(30 * time.Minute),
 	}
 }
 
@@ -35,13 +35,13 @@ func (c *Indexer) populateUnset() {
 	if c.CacheSize == 0 {
 		c.CacheSize = def.CacheSize
 	}
+	if c.GCInterval == 0 {
+		c.GCInterval = def.GCInterval
+	}
 	if c.ValueStoreDir == "" {
 		c.ValueStoreDir = def.ValueStoreDir
 	}
 	if c.ValueStoreType == "" {
 		c.ValueStoreType = def.ValueStoreType
-	}
-	if c.GCInterval == 0 {
-		c.GCInterval = def.GCInterval
 	}
 }
