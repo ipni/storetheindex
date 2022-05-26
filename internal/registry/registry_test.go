ffbf0af2eafb027e8b2e701ebc3b63add6cfc67e
gammazero <gammazero@users.noreply.github.com>
2022-05-26 16:48:37 -0700

    Fix test for low-res timers

--- internal/registry/registry_test.go
+++ internal/registry/registry_test.go
@@ -614,6 +614,8 @@ func TestPollProviderOverrides(t *testing.T) {
 		stopAfter:  0,
 	}
 	r.pollProviders(poll, overrides)
+	// wait some time to pass poll interval (for low-res timers)
+	time.Sleep(time.Second)
 	r.pollProviders(poll, overrides)
 	r.pollProviders(poll, overrides)
 
