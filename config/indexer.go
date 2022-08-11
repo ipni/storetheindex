90cf8099a482611211ae9288e884e1f59239b008
Will <will.scott@protocol.ai>
2022-08-11 12:32:54 +0000

    Allow init with a different STH bit prefix (#648)
    * Allow init with a different STH bit prefix

--- config/indexer.go
+++ config/indexer.go
@@ -23,6 +23,9 @@ type Indexer struct {
 	ValueStoreDir string
 	// Type of valuestore to use, such as "sth" or "pogreb".
 	ValueStoreType string
+	// bits for bucket size in store the hash. Note: this should not be changed
+	// from it's value at initialization or the datastore will be corrupted.
+	STHBits uint8
 }
 
 // NewIndexer returns Indexer with values set to their defaults.
@@ -34,6 +37,7 @@ func NewIndexer() Indexer {
 		ShutdownTimeout:     Duration(10 * time.Second),
 		ValueStoreDir:       "valuestore",
 		ValueStoreType:      "sth",
+		STHBits:             24,
 	}
 }
 
@@ -58,5 +62,8 @@ func (c *Indexer) populateUnset() {
 	}
 	if c.ValueStoreType == "" {
 		c.ValueStoreType = def.ValueStoreType
+	}
+	if c.STHBits == 0 {
+		c.STHBits = def.STHBits
 	}
 }
