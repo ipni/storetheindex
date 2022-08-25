502b61828840382584f8978ac76451a032455f5d
Masih H. Derkani <m@derkani.org>
2022-08-25 20:57:29 +0100

    Add script to get peer ID from identity file
    Add an script that accepts marshalled private key as stdin and prints
    the public key associated to that identity.
    
    Comment peer ID of dev and prod instances in code for convenience.

---
+++ scripts/peer_id_from_priv_key.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/peer"
+)
+
+// Reads marshalled crypto.PrivKey from os.stdin and prints the peer.ID that corresponds to the key.
+// See: crypto.UnmarshalPrivateKey, peer.IDFromPrivateKey.
+//
+// For example, you can directly get the peer.ID from a sops-encrypted identity file as long as
+// your terminal session is authenticated to the storetheindex AWS account and has access
+// to the relevant KMS keys like this:
+//
+//	sops -d indexer-0-identity.encrypted | go run <path>/<to>/peer_id_from_priv_key.go
+func main() {
+	all, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+	key, err := crypto.UnmarshalPrivateKey(all)
+	if err != nil {
+		panic(err)
+	}
+	pid, err := peer.IDFromPrivateKey(key)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(pid.String())
+}
