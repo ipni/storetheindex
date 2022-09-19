3515a83b588e4dc2c00495a6c84c7e791f64d591
Ivan Schasny <ivan.schasny@protocol.ai>
2022-09-19 11:24:20 +0100

    Added ipfs-index-provider to terraform configs

--- deploy/infrastructure/common/github_actions.tf
+++ deploy/infrastructure/common/github_actions.tf
@@ -52,6 +52,7 @@ module "github_actions_role" {
     "repo:filecoin-project/index-provider:*",
     "repo:filecoin-shipyard/index-observer:*",
     "repo:application-research/autoretrieve:*",
-    "repo:filecoin-shipyard/indexstar:*"
+    "repo:filecoin-shipyard/indexstar:*",
+    "repo:ipfs-shipyard/ipfs-index-provider:*"
   ]
 }
