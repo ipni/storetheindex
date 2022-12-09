c9e61beae5c38776638f1a229731e120870bf40c
gammazero <gammazero@users.noreply.github.com>
2022-12-08 18:16:58 -0800

    Use consistent port for libp2p listener
    Indexers all use port 3003 for their libp2p listener, and the assigner should use this also by default.
    
    - Update assigner to latestr image
    - Peer ber-indexer and cali-indexer to assigner

--- assigner/config/daemon.go
+++ assigner/config/daemon.go
@@ -5,8 +5,8 @@ type Daemon struct {
 	// HTTPAddr is the HTTP host multiaddr for receiving direct announce
 	// messages. Set to "none" to disable HTTP hosting.
 	HTTPAddr string
-	// P2PAddr is the libp2p host multiaddr for receiving announce messages. Set to "none" to
-	// disable libp2p hosting.
+	// P2PAddr is the libp2p host multiaddr for receiving announce messages.
+	// Set to "none" to disable libp2p hosting.
 	P2PAddr string
 	// NoResourceManager disables the libp2p resource manager when true.
 	NoResourceManager bool
@@ -16,7 +16,7 @@ type Daemon struct {
 func NewDaemon() Daemon {
 	return Daemon{
 		HTTPAddr: "/ip4/0.0.0.0/tcp/3001",
-		P2PAddr:  "/ip4/0.0.0.0/tcp/3000",
+		P2PAddr:  "/ip4/0.0.0.0/tcp/3003",
 	}
 }
 
