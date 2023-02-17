bb6660eba30b85f8413ecdaf2fe684f469a5e4a8
Masih H. Derkani <m@derkani.org>
2023-02-17 17:51:08 +0000

    Draft role definition and assumption

--- deploy/infrastructure/dev/us-east-2/s3_adstore.tf
+++ deploy/infrastructure/dev/us-east-2/s3_adstore.tf
@@ -1,31 +1,47 @@
-resource "aws_s3_bucket" "adstore" {
-  bucket = "adstore-bucket"
+#resource "aws_s3_bucket" "adstore" {
+#  bucket = "${local.environment_name}-sti-adstore"
+#
+#  tags = local.tags
+#}
+#
+#resource "aws_s3_bucket_acl" "adstore" {
+#  bucket = aws_s3_bucket.adstore.id
+#  acl    = "private"
+#}
 
-  tags = {
-    Name        = "advertisement car bucket"
-    Environment = "dev"
-  }
+module "sti_s3_rw_role" {
+  source  = "registry.terraform.io/terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "4.17.1"
+
+  create_role = true
+
+  role_name    = "${local.environment_name}_sti_s3_rw"
+  provider_url = module.eks.oidc_provider
+
+  role_policy_arns = [
+#    TODO find predefined ARN
+  ]
+
+  oidc_fully_qualified_subjects = ["system:serviceaccount:storetheindex:storetheindex"]
+
+  tags = local.tags
 }
 
-resource "aws_s3_bucket_acl" "adstore" {
-  bucket = aws_s3_bucket.adstore.id
-  acl    = "private"
-}
 
-resource "aws_s3_bucket_acl" "adstore" {
-  bucket = aws_s3_bucket.adstore.id
-  access_control_policy {
-    grant {
-      grantee {
-        id   = data.aws_canonical_user_id.current.id
-        type = "CanonicalUser"
-      }
-      permission = "READ"
-    }
-    grant {
-      owner {
-        id = data.aws_canonical_user_id.current.id
-      }
-    }
-  }
-}
+#resource "aws_s3_bucket_acl" "adstore" {
+#  bucket = aws_s3_bucket.adstore.id
+#  access_control_policy {
+#    grant {
+#      grantee {
+#        id   = data.aws_canonical_user_id.current.id
+#        type = "CanonicalUser"
+#      }
+#      permission = "READ"
+#    }
+#    grant {
+#      owner {
+#        id = data.aws_canonical_user_id.current.id
+#      }
+#    }
+#  }
+#}
