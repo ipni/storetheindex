0eece56af8a7ff2247f0fb2792ff2f60bc0f2340
Masih H. Derkani <m@derkani.org>
2023-02-17 18:56:53 +0000

    S3 set up and config

--- deploy/infrastructure/dev/us-east-2/s3_adstore.tf
+++ deploy/infrastructure/dev/us-east-2/s3_adstore.tf
@@ -1,21 +1,39 @@
-#resource "aws_s3_bucket" "adstore" {
-#  bucket = "${local.environment_name}-sti-adstore"
-#
-#  tags = local.tags
-#}
-#
-#resource "aws_s3_bucket_acl" "adstore" {
-#  bucket = aws_s3_bucket.adstore.id
-#  acl    = "private"
-#}
-
-module "s3-bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.7.0"
+resource "aws_s3_bucket" "sti_s3_adstore" {
+  bucket = "${local.environment_name}-sti-adstore"
+  tags   = local.tags
 }
 
+
+resource "aws_s3_bucket_acl" "adstore" {
+  bucket = aws_s3_bucket.sti_s3_adstore.id
+  acl    = "private"
+}
+
+data "aws_iam_policy_document" "sti_s3_rw" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      # TODO: Enable if needed.
+      #      "s3:DeleteObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [aws_s3_bucket.sti_s3_adstore.arn, "${aws_s3_bucket.sti_s3_adstore.arn}/*"]
+  }
+}
+
+resource "aws_iam_policy" "sti_s3_rw" {
+  name   = "${local.environment_name}_kust_ctrlr"
+  policy = data.aws_iam_policy_document.sti_s3_rw.json
+  tags   = local.tags
+}
+
+
 module "sti_s3_rw_role" {
-  source  = "registry.terraform.io/terraform-aws-modules/iam/aws/modules/iam-assumable-role-with-oidc"
+  source  = "registry.terraform.io/terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "4.17.1"
 
   create_role = true
@@ -24,7 +42,7 @@ module "sti_s3_rw_role" {
   provider_url = module.eks.oidc_provider
 
   role_policy_arns = [
-    arn:aws:iam::aws:policy/AmazonS3FullAccess
+    aws_iam_policy.sti_s3_rw.arn
   ]
 
   oidc_fully_qualified_subjects = ["system:serviceaccount:storetheindex:storetheindex"]
