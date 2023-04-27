bca5e88be4e294e39a9c1c5fad54b45a32d9521a
gammazero <gammazero@users.noreply.github.com>
2023-04-26 20:02:58 -0700

    update comments

--- revision/revision.go
+++ revision/revision.go
@@ -1,3 +1,5 @@
+// Package revision provides the rcs revision, embedded by the compiler, as a
+// global variable.
 package revision
 
 import (
@@ -6,19 +8,18 @@ import (
 )
 
 var (
-	// Revision is the rcs revision of this package.
+	// Revision is the rcs revision embedded by the compiler.
 	Revision string
-	// RevisionJSON is the rcs revision of this package, in JSON format.
+	// RevisionJSON is the rcs revision, in JSON format.
 	RevisionJSON []byte
 )
 
 func init() {
-	// If running from a module, try to get the build info.
+	// Get the rcs revision from the build info.
 	bi, ok := debug.ReadBuildInfo()
 	if !ok {
 		return
 	}
-	// Get the rcs revision.
 	for i := range bi.Settings {
 		if bi.Settings[i].Key == "vcs.revision" {
 			Revision = bi.Settings[i].Value
