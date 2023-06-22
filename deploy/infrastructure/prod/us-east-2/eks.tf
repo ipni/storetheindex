844e8696786a90e6079bfb0388b97d415c0614c8
Masih H. Derkani <m@derkani.org>
2023-06-22 13:14:55 +0100

    Move `inga` to instance types with more CPU
    `inga` is running out of CPU all the time. move it to c6a 12XL nodes.

--- deploy/infrastructure/prod/us-east-2/eks.tf
+++ deploy/infrastructure/prod/us-east-2/eks.tf
@@ -163,6 +163,17 @@ module "eks" {
         data.aws_subnet.ue2c1.id, data.aws_subnet.ue2c2.id, data.aws_subnet.ue2c3.id
       ]
     }
+    prod-ue2-c6a-12xl = {
+      min_size       = 0
+      max_size       = 20
+      desired_size   = 1
+      instance_types = ["c6a.12xlarge"]
+      subnet_ids     = [
+        data.aws_subnet.ue2a1.id, data.aws_subnet.ue2a2.id, data.aws_subnet.ue2a3.id,
+        data.aws_subnet.ue2b1.id, data.aws_subnet.ue2b2.id, data.aws_subnet.ue2b3.id,
+        data.aws_subnet.ue2c1.id, data.aws_subnet.ue2c2.id, data.aws_subnet.ue2c3.id
+      ]
+    }
   }
 }
 
