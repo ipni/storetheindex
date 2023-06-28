caf14a87c1e017a881c8771e5879611f74f3189f
Andrew Gillis <gammazero@users.noreply.github.com>
2023-06-28 01:55:26 -0700

    Update the github certificate thumbprints (#2033)
    * Update the github certificate thumbprints
    
    Looks like github changed certs, so we need a new fingerprint. Otherwise, ECR builds are faining fith error:
    
    "OpenIDConnect provider's HTTPS certificate doesn't match configured thumbprint"

--- deploy/infrastructure/common/github_actions.tf
+++ deploy/infrastructure/common/github_actions.tf
@@ -6,7 +6,8 @@ resource "aws_iam_openid_connect_provider" "github" {
   ]
 
   thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
   ]
   url = "https://token.actions.githubusercontent.com"
 }
