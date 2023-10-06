db873b154bb46903fb4db695bc6ce30c8b42f868
Andrew Gillis <gammazero@users.noreply.github.com>
2023-10-06 14:48:59 -0700

    Fix shutdown failure after manual sync (#2327)
    * Fix shutdown failure after manual sync
    
    If a manual synce request is given when there is already a sync in progress, the WaitGroup is being incremented even though a new goroutine is not started. This causes the server shutdown to hang waiting for the WaitGroup.
    
    * Do not call w.WriteHeader if calling w.Write and status is 200

--- internal/httpserver/util.go
+++ internal/httpserver/util.go
@@ -29,7 +29,9 @@ func MethodOK(w http.ResponseWriter, r *http.Request, method string) bool {
 
 func WriteJsonResponse(w http.ResponseWriter, status int, body []byte) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(status)
+	if status != http.StatusOK {
+		w.WriteHeader(status)
+	}
 	if _, err := w.Write(body); err != nil {
 		log.Errorw("cannot write response", "err", err)
 		http.Error(w, "", http.StatusInternalServerError)
--- server/admin/handler.go
+++ server/admin/handler.go
@@ -323,7 +323,6 @@ func (h *adminHandler) handlePostSyncs(w http.ResponseWriter, r *http.Request) {
 	log.Info("Syncing with peer")
 
 	// Start the sync, but do not wait for it to complete.
-	h.pendingSyncs.Add(1)
 	h.pendingSyncsLock.Lock()
 	if _, ok := h.pendingSyncsPeers[peerID]; ok {
 		h.pendingSyncsLock.Unlock()
@@ -334,6 +333,7 @@ func (h *adminHandler) handlePostSyncs(w http.ResponseWriter, r *http.Request) {
 	h.pendingSyncsPeers[peerID] = struct{}{}
 	h.pendingSyncsLock.Unlock()
 
+	h.pendingSyncs.Add(1)
 	go func() {
 		peerInfo := peer.AddrInfo{
 			ID:    peerID,
@@ -680,7 +680,6 @@ func (h *adminHandler) healthCheckHandler(w http.ResponseWriter, r *http.Request
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.WriteHeader(http.StatusOK)
 	if _, err := w.Write([]byte("\"OK\"")); err != nil {
 		log.Errorw("Cannot write HealthCheck response:", "err", err)
 	}
