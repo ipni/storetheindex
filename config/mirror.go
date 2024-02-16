a28cbe3df0e7b0211f3e51cd8678d0393c24d056
gammazero <gammazero@users.noreply.github.com>
2024-02-16 00:16:17 -0800

    Config comments

--- config/mirror.go
+++ config/mirror.go
@@ -9,17 +9,17 @@ import (
 // mirror is neither readable or writable, or a storage type is not specified,
 // then the mirror is not used.
 type Mirror struct {
-	// Read specifies to read advertisement content from the mirror.
+	// Read specifies to read advertisement content from the Retrieval mirror.
 	Read bool
-	// Write specifies to write advertisement content to the mirror.
+	// Write specifies to write advertisement content to the Storage mirror.
 	Write bool
 	// Compress specifies how to compress files. One of: "gzip", "none".
 	// Defaults to "gzip" if unspecified.
 	Compress string
-	// Storage configures the backing file store for the mirror write operations.
-	Storage filestore.Config
-	// Retrieval configures the backing file store for the mirror read operations.
+	// Retrieval configures the backing file store for mirror read operations.
 	Retrieval filestore.Config
+	// Storage configures the backing file store for mirror write operations.
+	Storage filestore.Config
 }
 
 // NewMirror returns Mirror with values set to their defaults.
