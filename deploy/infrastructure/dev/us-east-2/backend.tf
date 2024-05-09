5b19b8590a135fbd7db0d6a1576771caa3df367d
Masih H. Derkani <m@derkani.org>
2024-05-09 09:45:07 +0100

    Remove `dev` environment TF files
    Last remaining resources which belonged to dev environment are now
    removed. Remove the dev environment terraform files.

--- deploy/infrastructure/dev/us-east-2/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "sti-dev-terraform-state"
-    key    = "sti/dev/us-east-2/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
--- deploy/infrastructure/dev/us-east-2/route53.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_route53_zone" "dev_external" {
-  name = "dev.cid.contact"
-  tags = local.tags
-}
