961afe022ff5b0b961e0e5fc71a4183621b6f4dd
Andrew Gillis <11790789+gammazero@users.noreply.github.com>
2024-10-02 08:52:38 -0700

    Stop using github.com/mitchellh/go-homedir (#2666)
    This repo is archived, no longer needed, and no longer maintained.

--- fsutil/fsutil_test.go
+++ fsutil/fsutil_test.go
@@ -80,3 +80,36 @@ func TestFileExists(t *testing.T) {
 
 	require.True(t, fsutil.FileExists(fileName))
 }
+
+func TestExpandHome(t *testing.T) {
+	dir, err := fsutil.ExpandHome("")
+	require.NoError(t, err)
+	require.Equal(t, "", dir)
+
+	origDir := filepath.Join("somedir", "somesub")
+	dir, err = fsutil.ExpandHome(origDir)
+	require.NoError(t, err)
+	require.Equal(t, origDir, dir)
+
+	_, err = fsutil.ExpandHome(filepath.FromSlash("~nosuchuser/somedir"))
+	require.Error(t, err)
+
+	homeEnv := "HOME"
+	if runtime.GOOS == "windows" {
+		homeEnv = "USERPROFILE"
+	}
+	origHome := os.Getenv(homeEnv)
+	defer os.Setenv(homeEnv, origHome)
+	homeDir := filepath.Join(t.TempDir(), "testhome")
+	os.Setenv(homeEnv, homeDir)
+
+	const subDir = "mytmp"
+	origDir = filepath.Join("~", subDir)
+	dir, err = fsutil.ExpandHome(origDir)
+	require.NoError(t, err)
+	require.Equal(t, filepath.Join(homeDir, subDir), dir)
+
+	os.Unsetenv(homeEnv)
+	_, err = fsutil.ExpandHome(origDir)
+	require.Error(t, err)
+}
