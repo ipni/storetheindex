c7736be9f8704f221875de8f81c5cf0f579ba463
Masih H. Derkani <m@derkani.org>
2024-12-17 09:11:38 +0000

    Switch primary to berg now that disk is expanded and caught up
    Switch primary to berlin instance.
    
    Reduce min eks worker group size to zero as part of tearing down AWS
    environment.

--- deploy/infrastructure/prod/us-east-2/cloudfront.tf
+++ deploy/infrastructure/prod/us-east-2/cloudfront.tf
@@ -2,7 +2,7 @@ locals {
   indexstar_origin_id     = "${local.environment_name}_${local.region}_indexstar"
   indexstar_berg_origin_id     = "${local.environment_name}_${local.region}_indexstar_berg"
   indexstar_sf_origin_id     = "${local.environment_name}_${local.region}_indexstar_sf"
-  indexstar_primary = local.indexstar_sf_origin_id
+  indexstar_primary = local.indexstar_berg_origin_id
   http_announce_origin_id = "${local.environment_name}_${local.region}_assigner"
   cdn_subdomain           = "cdn"
   cf_log_bucket           = "${local.environment_name}-${local.region}-cf-log"
