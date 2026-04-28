92a894c46efd3a55714ed8e215ee86aea1b1acd7
Bartłomiej Święcki <bart@ipfs.io>
2026-04-28 10:17:38 +0200

    Remove dns iam

--- deploy/infrastructure/prod/us-east-2/outputs.tf
+++ deploy/infrastructure/prod/us-east-2/outputs.tf
@@ -10,10 +10,6 @@ output "kustomize_controller_role_arn" {
   value = module.kustomize_controller_role.iam_role_arn
 }
 
-output "external_dns_role_arn" {
-  value = module.external_dns_role.iam_role_arn
-}
-
 output "cert_manager_role_arn" {
   value = module.cert_manager_role.iam_role_arn
 }
